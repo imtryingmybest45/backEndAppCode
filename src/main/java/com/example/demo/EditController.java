95cb71f0448bc66cdc931e51328fdd901d18c960
unknown <sebokapril@gmail.com>
2026-02-07 13:39:37 -0700

    wow

--- src/main/java/com/example/demo/EditController.java
+++ src/main/java/com/example/demo/EditController.java
@@ -31,6 +31,7 @@ public class EditController {
         String movieNameAsEntered = editDTO.getMovieName(); //This is the movie name without spaces
         String movieReview = editDTO.getMovieReview();
         String origEditedName = editDTO.getOrigMovName();
+        String movieTier = editDTO.getMovieTier();
 
         String movieNameWithSpaces = movieNameAsEntered.substring(0, movieNameAsEntered.length());
         String movieNameWithoutSpaces = movieNameWithSpaces.replaceAll("\\s", ""); //movieName is the inputted name without spaces
@@ -40,20 +41,24 @@ public class EditController {
         String origEditedNameWithoutSpaces = origEditedNameWithSpaces.replaceAll("\\s", ""); //movieName is the inputted name without spaces
         origEditedNameWithoutSpaces = origEditedNameWithoutSpaces.replaceAll("[^a-zA-Z0-9]", "");
 
-        String newPagesFileContent = otherFunctions.writeNewPagesFile(movieNameWithSpaces, movieNameWithoutSpaces, movieReview);
+        String newPagesFileContent = otherFunctions.writeNewPagesFile(movieNameWithSpaces, movieNameWithoutSpaces, movieReview, movieTier);
 
         Map<String, String> filesContent = new HashMap<>();
         // Adding items
-        filesContent.put("src/pages/"+movieNameWithoutSpaces+".js", newPagesFileContent);
+        filesContent.put("src/pages/" + movieNameWithoutSpaces + ".js", newPagesFileContent);
 
-        if (!origEditedNameWithSpaces.equals(movieNameWithSpaces)){
+        String newHomeContent;
+        if (!origEditedNameWithSpaces.equals(movieNameWithSpaces)) {
             List<String> origNameList = otherFunctions.getOrigName();
             String origNameWithoutSpaces = origNameList.get(0);
             String origNameWithSpaces = origNameList.get(1);
-            String newHomeContent = otherFunctions.editRoutesAppFile(movieNameWithSpaces, movieNameWithoutSpaces, origEditedNameWithSpaces, origEditedNameWithoutSpaces);
-            newHomeContent = otherFunctions.editLinksAppFile(movieNameWithSpaces, movieNameWithoutSpaces, origEditedNameWithSpaces, origEditedNameWithoutSpaces, origNameWithSpaces, origNameWithoutSpaces, newHomeContent);
+            newHomeContent = otherFunctions.editRoutesAppFile(movieNameWithSpaces, movieNameWithoutSpaces, origEditedNameWithSpaces, origEditedNameWithoutSpaces);
+            newHomeContent = otherFunctions.editLinksAppFile(movieNameWithSpaces, movieNameWithoutSpaces, origEditedNameWithSpaces, origEditedNameWithoutSpaces, origNameWithSpaces, origNameWithoutSpaces, movieTier, newHomeContent);
             newHomeContent = otherFunctions.addImportLine(movieNameWithoutSpaces, origEditedNameWithoutSpaces, newHomeContent, submitFlag);
 
+            filesContent.put("src/pages/Home.js", newHomeContent);
+        } else {
+            newHomeContent = otherFunctions.editTier(movieNameWithSpaces, movieNameWithoutSpaces, origEditedNameWithSpaces, origEditedNameWithoutSpaces, movieTier);
             filesContent.put("src/pages/Home.js", newHomeContent);
         }
 
